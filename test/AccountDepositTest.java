0a9fdcfd1e4b3434f14d256f400b3878f871a3d1
Lucas Diogo <luc-diogo@outlook.com>
2019-08-23 15:39:23 -0300

    Finalizado V1 SDK Java

--- test/AccountDepositTest.java
+++ test/AccountDepositTest.java
@@ -21,7 +21,7 @@ public class AccountDepositTest extends TestCase {
     protected void setUp() {
 
         Config enviroment = new Config();
-        enviroment.SetEnviroment("5A3A044DE838403F9566BDFBEE9DE763");
+        enviroment.SetEnviroment("x-api-key");
     }
 
     @Test
